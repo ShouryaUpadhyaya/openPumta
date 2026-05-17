0d4e5abf45fad096e73c493728d34d26a56ac0d4
ShouryaUpadhyaya <shouryaupadhyaya79@gmail.com>
2026-05-17 18:24:57 +0530

    fix: error due to timer store change

--- next-app/app/components/Home/Clock.tsx
+++ next-app/app/components/Home/Clock.tsx
@@ -1,31 +1,18 @@
 import React from 'react';
 import { usePomodoroStore } from '@/store/usePomodoroStore';
-import { ConvertSecsToTimer, ConvertTimerToSecs } from '@/lib/utils';
+import { ConvertSecsToTimer } from '@/lib/utils';
 import ClockCircle from '../pomodoro/ClockCircle';
 import ClockTime from '../pomodoro/ClockTime';
-import { Button } from '@/components/ui/button';
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import ClockDialogBox from '../ClockDialogBox';
 
 function Clock() {
-  const { pomodoroTimer } = usePomodoroStore();
+  const { workDuration } = usePomodoroStore();
 
   const {
     hours: workHours,
     minutes: workMinutes,
     seconds: workSeconds,
-  } = ConvertSecsToTimer({ workSecs: pomodoroTimer, goalWorkSecs: pomodoroTimer });
+  } = ConvertSecsToTimer({ workSecs: workDuration });
 
   return (
     <section className="flex justify-center items-center">
